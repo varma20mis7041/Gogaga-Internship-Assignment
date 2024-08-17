e85dd16c71bb1cbd52d1776e395d61309cf1ebb7
Bhargav varma <80447853+varma20mis7041@users.noreply.github.com>
2024-08-17 13:45:35 +0530

    JS

--- index.js
+++ index.js
@@ -14,19 +14,13 @@ function onClickSubmit() {
 }
 
 
-
-
-// Open the modal
 btn.onclick = function() {
     modal.style.display = "block";
 }
 
-// Close the modal
 span.onclick = function() {
     modal.style.display = "none";
 }
-
-// Close modal when clicking outside of it
 window.onclick = function(event) {
     if (event.target == modal) {
         modal.style.display = "none";
